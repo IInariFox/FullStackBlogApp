84114e239acb1d220eefd770fe90811d5d6a9be2
nathon <nathonchavez01@gamil.com>
2022-01-30 22:01:28 +0900

    Finished app

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Category.create!(name: "Sports")
+Category.create!(name: "Physical fitness")
+Category.create!(name: "Lifestyle")
+Category.create!(name: "Food")
+Category.create!(name: "Fashion")
+Category.create!(name: "Music")
+Category.create!(name: "Business")
+Category.create!(name: "Finance")
+Category.create!(name: "Personal")
+Category.create!(name: "Travel")
+Category.create!(name: "Phylosophy")
+Category.create!(name: "Fun")
